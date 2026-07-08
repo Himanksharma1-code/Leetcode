ee63b08debb284739b01012e7e6062a12350a149
HIMANK SHARMA <himanksharma899@gmail.com>
2026-07-08 13:02:43 +0530

    0 ms (100%) 12.4 MB (20.14%) Easy

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,6 +1,6 @@
 class Solution:
     def romanToInt(self, s):
-        # Map of Roman numerals to their integer values
+       
         roman_map = {
             'I': 1,
             'V': 5,
@@ -14,14 +14,14 @@ class Solution:
         total = 0
         prev_value = 0
         
-        # Iterate over the string from right to left
+       
         for char in reversed(s):
             value = roman_map[char]
-            # If the current value is less than the previous value, subtract it
+            
             if value < prev_value:
                 total -= value
             else:
-                # Otherwise, add it
+                
                 total += value
             prev_value = value
         
