fe75c06fa431a7a646827525128d1c06ab528f3b
HIMANK SHARMA <himanksharma899@gmail.com>
2026-09-16 23:04:52 +0530

    14 ms (11.3%) 12.4 MB (62.05%) Hard

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def totalNQueens(self, n):
+        cols = set()
+        pos_diag = set()
+        neg_diag = set()
+
+        def backtrack(r):
+            if r == n:
+                return 1
+
+            count = 0
+            for c in range(n):
+                if c in cols or (r + c) in pos_diag or (r - c) in neg_diag:
+                    continue
+
+                cols.add(c)
+                pos_diag.add(r + c)
+                neg_diag.add(r - c)
+
+                count += backtrack(r + 1)
+
+                cols.remove(c)
+                pos_diag.remove(r + c)
+                neg_diag.remove(r - c)
+
+            return count
+
+        return backtrack(0)
