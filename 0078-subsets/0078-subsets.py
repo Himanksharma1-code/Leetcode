52189b9b85eee2d9ecd53240193471eaabe23f1c
HIMANK SHARMA <himanksharma899@gmail.com>
2026-09-23 23:10:56 +0530

    0 ms (100%) 12.7 MB (25.12%) Medium

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subsets(self, nums):
+        res = []
+
+        def backtrack(start, current):
+            res.append(list(current))
+            for i in range(start, len(nums)):
+                current.append(nums[i])
+                backtrack(i + 1, current)
+                current.pop()
+
+        backtrack(0, [])
+        return res
